c21a69d3fc069b09fbe30737bb9d60054f9ea3e4
Yash Kakrecha <yashvk.inexture@gmail.com>
2024-04-19 12:33:14 +0530

    Project Setup

---
+++ src/main/java/com/example/leetcodecheaterwebscrapper/LeetcodeCheaterWebscrapperApplication.java
@@ -0,0 +1,13 @@
+package com.example.leetcodecheaterwebscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeCheaterWebscrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeCheaterWebscrapperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/leetcodecheaterwebscrapper/LeetcodeCheaterWebscrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcodecheaterwebscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeCheaterWebscrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
